ef9a7504927b179f2b3b3f9bf42c7c507d1027fa
Muneera <mborshai>
2014-12-05 09:22:44 +0300

    Finish sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  describe "signin" do
+
+    describe "with valid information" do
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
+  end
+end
